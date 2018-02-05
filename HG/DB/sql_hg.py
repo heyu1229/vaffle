8d469a7c85e1ac01e21a984ecdad6ebfa38524e3
heyu1229 <31230618+heyu1229@users.noreply.github.com>
2018-02-05 15:24:49 +0800

    Add files via upload

---
+++ HG/DB/sql_hg.py
@@ -0,0 +1,40 @@
+import sys,time
+'''
+数据库字段add_order  -> auto_type
+'automation'=>'普通自动化','automatic_order'=>'自动下单',
+'''
+
+
+class SQL_HG():
+    #status 状态(0: 新建提交, 1: 执行过程中, 2: 执行成功, 3: 执行失败, 4: 删除)
+    def get_task_add_order(self):
+        #add_order  1：下单和取消下单流程执行 0：下单和取消下单流程不执行
+        sql = "select auto_type from task_sync where project_id = 3 and status=0 order by id desc limit 1"
+        return  sql
+
+    def get_task_id(self):
+
+        sql = "select id from task_sync where project_id = 3 and status=0 order by id desc limit 1"
+        return  sql
+
+    def get_task_add_order1(self):
+        #status已被改为1
+        sql = "select auto_type from task_sync where project_id = 3 and status=1 order by id desc limit 1"
+        return  sql
+
+
+    def update_task_execute(self):
+        nowTime = time.strftime("%Y-%m-%d %H:%M:%S")
+        print(nowTime)
+        sql = "update task_sync set status = 1,updated_at='"+nowTime+"' where project_id = 3 and status=0 order by id desc limit 1"
+        return sql
+
+    def update_task_finish(self):
+        nowTime = time.strftime("%Y-%m-%d %H:%M:%S")
+        sql = "update task_sync set status = 2,updated_at='"+nowTime+"' where project_id = 3 and status=1 order by id desc limit 1"
+        return sql
+
+    def update_task_fail(self):
+        sql = "update task_sync set status = 3,updated_at= where project_id = 3 and status=1"
+
+#SQL_TEST_1().sql_test()
