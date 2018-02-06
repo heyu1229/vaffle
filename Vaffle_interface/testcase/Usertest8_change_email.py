5cfda03b1fa82b6ccb68d6996978607fd9b52895
heyu1229 <1004856404@qq.com>
2018-02-06 13:30:44 +0800

    heyu

--- Vaffle_interface/testcase/Usertest8_change_email.py
+++ Vaffle_interface/testcase/Usertest8_change_email.py
@@ -12,71 +12,32 @@ from get_url import Url
 from read_data import Read_ExcelData
 from write_data import Write_ExcelData
 from get_version import Version
+from func_requests import FuncRequests
 #---------------修改用户邮箱----------------------
 class ChangeEmail(unittest.TestCase):
 
     def setUp(self):
-
-        #路径
-        self.url = Url().test_url()
         self.member_id = '760'
-        #获取EXcel路径
-        self.path = Url().test_path()
-        # 打开excel
-        self.filename = "aa"
-        self.obi = Read_ExcelData ()
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
+        self.requests = FuncRequests()
     #-----------------修改用户邮箱成功----------------------------------
     def testcase_001(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 0, 40, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 0, 40, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version':self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 0, 40, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 0, 40, 7, result['msg'],self.path,self.filename )
-        self.obj.write_excel_data ( 0, 40, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 0, 40, 9, end - start,self.path,self.filename )
-
+        sheet_index =0
+        row = 40
+        print("testcase001 修改用户邮箱成功：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
-
         self.assertEqual('', result['msg'])
         print("msg返回值：ok")
 
     #-----------------修改为已存在的用户邮箱----------------------------------
-
     def testcase_002(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 0, 41, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 0, 41, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 0, 41, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 0, 41, 7, result['msg'] ,self.path,self.filename)
-        self.obj.write_excel_data ( 0, 41, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 0, 41, 9, end - start ,self.path,self.filename)
-
-
+        sheet_index =0
+        row = 41
+        print("testcase002 修改为已存在的用户邮箱：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(10002, result['code'])
         print("code返回值：10002")
-
         self.assertEqual('The email address has been taken.', result['msg'])
         print("msg返回值：The email address has been taken.")
 
@@ -84,23 +45,10 @@ class ChangeEmail(unittest.TestCase):
     #-----------------修改的邮箱格式不正确----------------------------------
 
     def testcase_003(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 0, 42, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 0, 42, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 0, 42, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 0, 42, 7, result['msg'] ,self.path,self.filename)
-        self.obj.write_excel_data ( 0, 42, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 0, 42, 9, end - start,self.path,self.filename )
-
+        sheet_index =0
+        row = 42
+        print("testcase003 修改的邮箱格式不正确：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(9999, result['code'])
         print("code返回值：9999")
 
