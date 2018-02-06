8b1b4c2e805e8af64e12e53785fc16606d19831c
heyu1229 <1004856404@qq.com>
2018-02-06 12:00:16 +0800

    heyu

--- Vaffle_interface/public_1/func_requests.py
+++ Vaffle_interface/public_1/func_requests.py
@@ -15,20 +15,17 @@ class FuncRequests():
 
     #-----------------接口请求（payload从文件中读取）----------------------------------
     def interface_requests(self,member_id,sheet_index,row):
-
         # 获取EXcel路径
         self.path = Url().test_path()
-        # 打开excel
-        self.filename = xlrd.open_workbook(self.path)
 
         # 路径
         url = Url().test_url()
         self.obi = Read_ExcelData()
-        self.base_url = self.obi.read_excel_data(sheet_index, row, 4, self.filename)
+        self.base_url = self.obi.read_excel_data(sheet_index, row, 4)
         self.base_url1 = url + self.base_url
         # 获取版本
         self.version = Version().test_version()
-        payload = self.obi.read_excel_data_dict(sheet_index, row, 5,self.filename)
+        payload = self.obi.read_excel_data_dict(sheet_index, row, 5)
         #获取token值
         token = Token().test_token1(payload, member_id)
 
@@ -45,14 +42,14 @@ class FuncRequests():
         # 写入excel中
         str_result = str(result)
         self.obj = Write_ExcelData()
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path,self.filename)
+        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path)
+        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path)
         if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path)
+            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path)
         else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path)
+        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path)
         return result
 
     # -----------------接口请求（payload动态获取）----------------------------------
@@ -60,12 +57,11 @@ class FuncRequests():
 
         # 获取EXcel路径
         self.path = Url().test_path()
-        # 打开excel
-        self.filename = xlrd.open_workbook(self.path)
+
         # 路径
         url = Url().test_url()
         self.obi = Read_ExcelData()
-        self.base_url = self.obi.read_excel_data(sheet_index, row, 4, self.filename)
+        self.base_url = self.obi.read_excel_data(sheet_index, row, 4)
         self.base_url1 = url + self.base_url
         # 获取版本
         self.version = Version().test_version()
@@ -86,12 +82,12 @@ class FuncRequests():
         # 写入excel中
         str_result = str(result)
         self.obj = Write_ExcelData()
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path,self.filename)
+        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path)
+        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path)
         if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path)
+            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path)
         else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path)
+        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path)
         return result
