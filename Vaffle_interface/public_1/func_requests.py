a7ccd252126d0ddbc0a1b9dcdba83c8392bee796
heyu1229 <1004856404@qq.com>
2018-02-06 09:50:09 +0800

    heyu

--- Vaffle_interface/public_1/func_requests.py
+++ Vaffle_interface/public_1/func_requests.py
@@ -14,7 +14,7 @@ from get_version import Version
 class func_requests():
 
     #-----------------接口请求（payload从文件中读取）----------------------------------
-    def interface_requests(self,sheet_index,row):
+    def interface_requests(self,member_id,sheet_index,row):
 
         # 获取EXcel路径
         self.path = Url().test_path()
@@ -23,13 +23,13 @@ class func_requests():
 
         # 路径
         url = Url().test_url()
-        self.base_url = Read_ExcelData().read_excel_data(sheet_index, row, 4, self.filename)
+        self.obi = Read_ExcelData()
+        self.base_url = self.obi.read_excel_data(sheet_index, row, 4, self.filename)
         self.base_url1 = url + self.base_url
         # 获取版本
         self.version = Version().test_version()
-        payload = Read_ExcelData().read_excel_data_dict(sheet_index, row, 5,self.filename)
+        payload = self.obi.read_excel_data_dict(sheet_index, row, 5,self.filename)
         #获取token值
-        member_id = "744"
         token = Token().test_token1(payload, member_id)
 
         #记录接口的请求时间
@@ -44,18 +44,19 @@ class func_requests():
 
         # 写入excel中
         str_result = str(result)
-        Write_ExcelData().write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        Write_ExcelData().write_excel_data(sheet_index, row, 10, end - start,self.path,self.filename)
+        self.obj = Write_ExcelData()
+        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
+        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path,self.filename)
         if len(str_result) > 32767:
-            Write_ExcelData().write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            Write_ExcelData().write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
         else:
-            Write_ExcelData().write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-        Write_ExcelData().write_excel_data(sheet_index, row, 7, "ok",self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
+        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path,self.filename)
         return result["code"]
 
     # -----------------接口请求（payload动态获取）----------------------------------
-    def interface_requests_payload(self,sheet_index,row,payload):
+    def interface_requests_payload(self,member_id,sheet_index,row,payload):
 
         # 获取EXcel路径
         self.path = Url().test_path()
@@ -63,13 +64,13 @@ class func_requests():
         self.filename = xlrd.open_workbook(self.path)
         # 路径
         url = Url().test_url()
-        self.base_url = Read_ExcelData().read_excel_data(sheet_index, row, 4, self.filename)
+        self.obi = Read_ExcelData()
+        self.base_url = self.obi.read_excel_data(sheet_index, row, 4, self.filename)
         self.base_url1 = url + self.base_url
         # 获取版本
         self.version = Version().test_version()
         payload = payload
         #获取token值
-        member_id = "744"
         token = Token().test_token1(payload, member_id)
 
         # 记录接口的请求时间
@@ -84,12 +85,13 @@ class func_requests():
 
         # 写入excel中
         str_result = str(result)
-        Write_ExcelData().write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        Write_ExcelData().write_excel_data(sheet_index, row, 10, end - start,self.path,self.filename)
+        self.obj = Write_ExcelData()
+        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
+        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path,self.filename)
         if len(str_result) > 32767:
-            Write_ExcelData().write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            Write_ExcelData().write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
         else:
-            Write_ExcelData().write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-        Write_ExcelData().write_excel_data(sheet_index, row, 7, "ok",self.path,self.filename)
+            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
+        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path,self.filename)
         return result["code"]
