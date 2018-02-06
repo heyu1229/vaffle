fb931a92815435f0013843047996b1e0ca0c46ee
heyu1229 <1004856404@qq.com>
2018-02-06 16:10:23 +0800

    heyu

--- Vaffle_interface/testcase/Discover_test4_discover_hotsearch.py
+++ Vaffle_interface/testcase/Discover_test4_discover_hotsearch.py
@@ -6,6 +6,7 @@ import time,gc
 import xlrd,sys
 import global_list
 sys.path.append(global_list.path+"/public_1")
+from func_requests import FuncRequests
 from get_token import Token
 from get_url import Url
 from read_data import Read_ExcelData
@@ -16,39 +17,15 @@ from get_version import Version
 class Discover(unittest.TestCase):
 
     def setUp(self):
-
-        #路径
-        self.url = Url().test_url()
         self.member_id = '744'
-
-        #获取EXcel路径
-        self.path = Url().test_path()
-        self.filename = "aa"
-        self.obi = Read_ExcelData()
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
+        self.requests = FuncRequests()
 
     #-----------------热门搜索hot searches----------------------------------
     def testcase_001(self):
-
-        #获取路径
-        url1 = self.obi.read_excel_data ( 4, 4, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 4, 4, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version':self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 4, 4, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 4, 4, 7, result['msg'],self.path,self.filename)
-        self.obj.write_excel_data ( 4, 4, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 4, 4, 9, end - start ,self.path,self.filename)
-
+        sheet_index =4
+        row = 4
+        print("testcase001 发热门搜索hot searches：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
 
