b9ef9c7088555a584d4ab3c87508e2576d667db3
yuqin <921467314@qq.com>
2018-02-06 16:01:23 +0800

    2

--- Vaffle_interface/testcase/Search_test3_search_integrate.py
+++ Vaffle_interface/testcase/Search_test3_search_integrate.py
@@ -12,151 +12,57 @@ from get_token import Token
 from read_data import Read_ExcelData
 from write_data import Write_ExcelData
 from get_version import Version
+from func_requests import FuncRequests
+
 #---------------综合搜索功能----------------------
 class Passport_Login(unittest.TestCase):
 
     def setUp(self):
+        self.r = FuncRequests()
 
-        #路径
-        url = Url().test_url()
-        #获取EXcel路径
-        self.path = Url().test_path()
-        # 打开excel
-        self.filename = "aa"
-        self.obi = Read_ExcelData ()
-        self.base_url = self.obi.read_excel_data(9, 7, 4,self.filename)
-        self.base_url1 = url + self.base_url
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
     # -----------------搜索post数据----------------------------------
     def testcase_001(self):
         sheet_index = 9
         row = 7
         print("testcase_001搜索post数据：")
-        payload = self.obi.read_excel_data_dict(sheet_index, row, 5,self.filename)
-        print(payload)
-        # 获取token值`
         member_id = "744"
-        token = Token().test_token1(payload,member_id)
-        start = time.time ()
-        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
-                   "login": member_id}
-        r = requests.post(self.base_url1,params=payload,headers=headers)
-        print(self.base_url1)
-        result=r.json()
-        print(result)
-        end = time.time ()
-        # 写入excel中
-        Write_ExcelData ().write_excel_data ( sheet_index, row, 10, end - start,self.path,self.filename )
-        str_result = str(result)
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
-        else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
+        result=self.r.interface_requests(member_id,sheet_index,row)
 
         self.assertEqual(10000, result["code"])
         print("code返回值：10000")
-        self.obj.write_excel_data(sheet_index, row, 7, "ok",self.path,self.filename)
-
 
      # -----------------搜索reveal数据----------------------------------
     def testcase_002(self):
         sheet_index = 9
         row = 8
         print("testcase_002搜索reveal数据：")
-        payload=self.obi.read_excel_data_dict(sheet_index, row, 5, self.filename)
-        # 获取token值
         member_id = "744"
-        token = Token().test_token1(payload,member_id)
-        start = time.time ()
-        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
-                   "login": member_id}
-        r = requests.post(self.base_url1, params=payload, headers=headers)
-        result = r.json()
-        print(result)
-        end = time.time ()
-        # 写入excel中
-        Write_ExcelData ().write_excel_data ( sheet_index, row, 10, end - start,self.path,self.filename )
-        str_result = str(result)
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
-        else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-
+        result=self.r.interface_requests(member_id,sheet_index,row)
 
         self.assertEqual(10000, result["code"])
         print("code返回值：10000")
-        self.obj.write_excel_data(sheet_index, row, 7, "ok",self.path,self.filename)
-
 
      # -----------------搜索people数据---------------------------------
     def testcase_003(self):
         sheet_index = 9
         row = 9
         print("testcase_003搜索people数据：")
-        payload = self.obi.read_excel_data_dict(sheet_index, row, 5,self.filename)
-        # 获取token值
         member_id = "744"
-        token = Token().test_token1(payload,member_id)
-        start = time.time ()
-        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
-                   "login": member_id}
-        r = requests.post(self.base_url1, params=payload, headers=headers)
-        result = r.json()
-        print(result)
-        end = time.time ()
-        # 写入excel中
-        Write_ExcelData ().write_excel_data ( sheet_index, row, 10, end - start,self.path,self.filename )
-        str_result = str(result)
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
-        else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-
+        result=self.r.interface_requests(member_id,sheet_index,row)
 
         self.assertEqual(10000, result["code"])
         print("code返回值：10000")
-        self.obj.write_excel_data(sheet_index, row, 7, "ok",self.path,self.filename)
 
      # -----------------keywords为空---------------------------------
     def testcase_006(self):
         sheet_index = 9
         row = 10
         print("testcase_006keywords为空：")
-        payload = self.obi.read_excel_data_dict(sheet_index, row, 5, self.filename)
-        # 获取token值
         member_id = "744"
-        token = Token().test_token1(payload,member_id)
-        start = time.time ()
-        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
-                   "login": member_id}
-        r = requests.post(self.base_url1, params=payload, headers=headers)
-        result = r.json()
-        print(result)
-        end = time.time ()
-        # 写入excel中
-        Write_ExcelData ().write_excel_data ( sheet_index, row, 10, end - start,self.path,self.filename )
-        str_result = str(result)
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path,self.filename)
-        if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
-        else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-
+        result=self.r.interface_requests(member_id,sheet_index,row)
 
         self.assertEqual(9999, result["code"])
         print("code返回值：9999")
-        self.assertEqual("Time out.", result["msg"])
-        print("msg返回值：Time out.")
 
 if __name__=="__main__":
     unittest.main()
