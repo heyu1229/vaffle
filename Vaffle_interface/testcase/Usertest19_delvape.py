90dae39e2470b4a3967816b5670c2440fbde5490
heyu1229 <1004856404@qq.com>
2018-02-06 14:42:35 +0800

    heyu

--- Vaffle_interface/testcase/Usertest19_delvape.py
+++ Vaffle_interface/testcase/Usertest19_delvape.py
@@ -4,14 +4,9 @@ import requests
 import json,gc,sys
 import global_list
 sys.path.append(global_list.path+"/public_1")
-import time
-
-import xlrd
-from get_token import Token
 from get_url import Url
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from get_version import Version
+from func_requests import FuncRequests
+
 #------------------------电子烟设备删除---------------------------
 class Delvape(unittest.TestCase):
 
@@ -19,36 +14,16 @@ class Delvape(unittest.TestCase):
         #路径
         self.url = Url().test_url()
         self.member_id = '744'
-        #获取EXcel路径
-        self.path = Url().test_path()
-        # 打开excel
-        self.filename = "aa"
-        self.obi = Read_ExcelData ()
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
+        self.requests = FuncRequests()
     #-----------------电子烟设备批量删除--vape_member_vape_device表--------------------------------
     def testcase_001(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 0, 70, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
+        sheet_index =0
+        row = 70
+        print("testcase001 电子烟设备批量删除：")
         obj = ('960','961')
         top_ids = json.dumps(obj)
         payload = {'ids': top_ids}
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 0, 70, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 0, 70, 7, result['msg'] ,self.path,self.filename)
-        self.obj.write_excel_data ( 0, 70, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 0, 70, 9, end - start,self.path,self.filename )
-
-
+        result = self.requests.interface_requests_payload(self.member_id,sheet_index,row,payload)
         self.assertEqual ( 10000, result['code'] )
         print ( "code返回值：10000" )
         self.assertEqual ( '', result['msg'] )
