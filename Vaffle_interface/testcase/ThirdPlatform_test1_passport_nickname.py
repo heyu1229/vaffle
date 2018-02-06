9e2fc9c35543bdd88f2d19834b3a70d496874bf0
yuqin <921467314@qq.com>
2018-02-06 15:55:40 +0800

    2

--- Vaffle_interface/testcase/ThirdPlatform_test1_passport_nickname.py
+++ Vaffle_interface/testcase/ThirdPlatform_test1_passport_nickname.py
@@ -12,57 +12,28 @@ from get_token import Token
 from read_data import Read_ExcelData
 from write_data import Write_ExcelData
 from get_version import Version
+from func_requests import FuncRequests
+
 #---------------检测第三方是否注册----------------------
 class Passport_Login(unittest.TestCase):
 
     def setUp(self):
+        self.r = FuncRequests()
 
-        #路径
-        url = Url().test_url()
-        #获取EXcel路径
-        self.path = Url().test_path()
-        # 打开excel
-        self.filename = "aa"
-        self.obi = Read_ExcelData ()
-        self.base_url = self.obi.read_excel_data(6, 1, 4,self.filename)
-        self.base_url1 = url + self.base_url
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
     #-----------------	检测第三方已注册----------------------------------
     #-----------------facebook:411568645883504 twitter:861855853032357889  vk:427871220--------------------------
     def testcase_001(self):
         sheet_index = 6
         row = 1
         print("testcase_001检测第三方已注册：")
-        payload = self.obi.read_excel_data_dict(sheet_index, row, 5,self.filename)
-        # 获取token值
-        token = Token().test_token(payload)
-        start = time.time ()
-        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
-                   "login": "none"}
-        r = requests.post(self.base_url1,params=payload,headers=headers)
-        result=r.json()
-        data=result["data"]
-        print(result)
-        end = time.time ()
-        # 写入excel中
-        self.obj.write_excel_data ( sheet_index, row, 10, end - start ,self.path,self.filename)
-        str_result = str(result)
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
-        else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-
-
+        member_id='none'
+        result=self.r.interface_requests(member_id,sheet_index,row)
+        data=result['data']
         self.assertEqual(10000, result["code"])
         print("code返回值：10000")
 
         self.assertEqual(1, len(data["list"]))
         print("推荐的用户昵称等于1，表示已经注册")
-        self.obj.write_excel_data(sheet_index, row, 7, "推荐的用户昵称等于1，表示已经注册",self.path,self.filename)
 
 
     #-----------------	检测第三方未注册----------------------------------
@@ -71,33 +42,14 @@ class Passport_Login(unittest.TestCase):
         sheet_index = 6
         row = 2
         print("testcase_001检测第三方未注册：")
-        payload = self.obi.read_excel_data_dict(sheet_index, row, 5,self.filename)
-        # 获取token值
-        token = Token().test_token(payload)
-        start = time.time ()
-        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
-                   "login": "none"}
-        r = requests.post(self.base_url1,params=payload,headers=headers)
-        result=r.json()
+        member_id = 'none'
+        result = self.r.interface_requests(member_id, sheet_index, row)
         data=result["data"]
-        print(result)
-        end = time.time ()
-        # 写入excel中
-        self.obj.write_excel_data ( sheet_index, row, 10, end - start,self.path,self.filename )
-        str_result = str(result)
-        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path,self.filename)
-        if len(str_result) > 32767:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path,self.filename)
-            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path,self.filename)
-        else:
-            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path,self.filename)
-
 
         self.assertEqual(10000, result["code"])
         print("code返回值：10000")
         self.assertEqual(5, len(data["list"]))
         print("推荐的用户昵称不等于1，表示未注册")
-        self.obj.write_excel_data(sheet_index, row, 7, "推荐的用户昵称不等于1，表示未注册",self.path,self.filename)
 
 
 
