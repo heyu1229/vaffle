fd8b05e03b7a99b3bd4c4f27aa69730d55e49d47
heyu1229 <1004856404@qq.com>
2018-02-06 16:38:02 +0800

    heyu

--- Vaffle_interface/testcase/Points_test1_membership.py
+++ Vaffle_interface/testcase/Points_test1_membership.py
@@ -6,46 +6,19 @@ import time,gc
 import xlrd,sys
 import global_list
 sys.path.append(global_list.path+"/public_1")
-from get_token import Token
-from get_url import Url
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from get_version import Version
+from func_requests import FuncRequests
 #---------------积分等级首页----------------------
 class Membership(unittest.TestCase):
 
     def setUp(self):
-
-        #路径
-        self.url = Url().test_url()
         self.member_id = '744'
-
-        #获取EXcel路径
-        self.path = Url().test_path()
-        # 打开excel
-        self.filename = "aa"
-        self.obi = Read_ExcelData ()
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
+        self.requests = FuncRequests()
     #-----------------积分等级首页----------------------------------
     def testcase_001(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 8, 1, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 8, 1, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 8, 1, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 8, 1, 7, result['msg'],self.path,self.filename )
-        self.obj.write_excel_data ( 8, 1, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 8, 1, 9, end - start ,self.path,self.filename)
+        sheet_index =8
+        row = 1
+        print("testcase001 积分等级首页：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
 
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
--- Vaffle_interface/testcase/Points_test3_rewards_records.py
+++ Vaffle_interface/testcase/Points_test3_rewards_records.py
@@ -6,47 +6,19 @@ import time,gc
 import xlrd,sys
 import global_list
 sys.path.append(global_list.path+"/public_1")
-from get_token import Token
-from get_url import Url
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from get_version import Version
+from func_requests import FuncRequests
 #---------------积分流水记录-type:all、add、reduce---------------------
 class RewardsDescription(unittest.TestCase):
 
     def setUp(self):
-
-        #路径
-        self.url = Url().test_url()
         self.member_id = '744'
-
-        #获取EXcel路径
-        self.path = Url().test_path()
-        # 打开excel
-        self.filename = "aa"
-        self.obi = Read_ExcelData ()
-        # 获取版本
-        self.version = Version ().test_version ()
-        self.obj = Write_ExcelData()
+        self.requests = FuncRequests()
     #-----------------积分流水记录-all---------------------------------
     def testcase_001(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 8, 3, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 8, 3, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 8, 3, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 8, 3, 7, result['msg'] ,self.path,self.filename)
-        self.obj.write_excel_data ( 8, 3, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 8, 3, 9, end - start,self.path,self.filename )
-
+        sheet_index =8
+        row = 3
+        print("testcase001 积分流水记录-all：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
         self.assertEqual('', result['msg'])
@@ -55,23 +27,10 @@ class RewardsDescription(unittest.TestCase):
 
     #-----------------积分流水记录-add---------------------------------
     def testcase_002(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 8, 4, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 8, 4, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 8, 4, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 8, 4, 7, result['msg'] ,self.path,self.filename)
-        self.obj.write_excel_data ( 8, 4, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 8, 4, 9, end - start,self.path,self.filename )
-
+        sheet_index =8
+        row = 4
+        print("testcase002 积分流水记录-add：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
         self.assertEqual('', result['msg'])
@@ -80,23 +39,10 @@ class RewardsDescription(unittest.TestCase):
 
     #-----------------积分流水记录-reduce---------------------------------
     def testcase_003(self):
-        #获取路径
-        url1 = self.obi.read_excel_data ( 8, 5, 4,self.filename)
-        #原地址+当前接口地址拼接
-        url1 = self.url +url1
-        payload =self.obi.read_excel_data_dict ( 8, 5, 5,self.filename)
-        token = Token ().test_token1 ( payload,self.member_id )
-        start = time.time ()
-        headers = {'device': 'android ', 'version': self.version, 'lang': 'en', 'timestamp': '1493780505', 'token':token,'login': self.member_id}
-        r = requests.post ( url1,params=payload,headers=headers )
-        result=r.json()
-        print(result)
-        end = time.time ()
-        self.obj.write_excel_data( 8, 5, 6, result['code'],self.path,self.filename)
-        self.obj.write_excel_data ( 8, 5, 7, result['msg'],self.path,self.filename )
-        self.obj.write_excel_data ( 8, 5, 8, str(result),self.path,self.filename)
-        self.obj.write_excel_data ( 8, 5, 9, end - start,self.path,self.filename )
-
+        sheet_index =8
+        row = 5
+        print("testcase003 积分流水记录-reduce：")
+        result = self.requests.interface_requests(self.member_id,sheet_index,row)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
 
