e63a87c43d47f80c12f0d9b4d35e2955c3b65b6e
heyu1229 <1004856404@qq.com>
2018-02-06 15:35:06 +0800

    heyu

--- Vaffle_interface/public_1/func_requests.py
+++ Vaffle_interface/public_1/func_requests.py
@@ -13,7 +13,7 @@ from get_version import Version
 #---------------关注----------------------
 class FuncRequests():
 
-    #-----------------接口请求（payload从文件中读取）----------------------------------
+    #-----------------接口请求（payload从文件中读取,且调用read_excel_data_dict方法）----------------------------------
     def interface_requests(self,member_id,sheet_index,row):
         # 获取EXcel路径
         self.path = Url().test_path()
@@ -91,6 +91,47 @@ class FuncRequests():
             self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path)
         self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path)
         return result
+
+
+    #-----------------接口请求（payload从文件中读取,且调用read_excel_data方法）----------------------------------
+    def interface_requests2(self,member_id,sheet_index,row):
+        # 获取EXcel路径
+        self.path = Url().test_path()
+
+        # 路径
+        url = Url().test_url()
+        self.obi = Read_ExcelData()
+        self.base_url = self.obi.read_excel_data(sheet_index, row, 4)
+        self.base_url1 = url + self.base_url
+        # 获取版本
+        self.version = Version().test_version()
+        payload = self.obi.read_excel_data(sheet_index, row, 5)
+        #获取token值
+        token = Token().test_token1(payload, member_id)
+
+        #记录接口的请求时间
+        start = time.time()
+        headers = {"device": "android ", "version": self.version, "lang": "en", "timestamp": "1493780505", "token": token,
+                   "login": member_id,"serial_number":"48525687125863258471123568955554","company":"HUAWEI","phone_model":"P10","system_version":"system_version"}
+        r = requests.post(self.base_url1, params=payload, headers=headers)
+        result = r.json()
+        format_result = json.dumps(result, indent=1)
+        print(format_result)
+        end = time.time()
+
+        # 写入excel中
+        str_result = str(result)
+        self.obj = Write_ExcelData()
+        self.obj.write_excel_data(sheet_index, row, 6, result["code"],self.path)
+        self.obj.write_excel_data(sheet_index, row, 7, result["msg"],self.path)
+        if len(str_result) > 32767:
+            self.obj.write_excel_data(sheet_index, row, 8, str_result[0:32767],self.path)
+            self.obj.write_excel_data(sheet_index, row, 9, str_result[32767:],self.path)
+        else:
+            self.obj.write_excel_data(sheet_index, row, 8, str_result,self.path)
+        self.obj.write_excel_data(sheet_index, row, 10, end - start,self.path)
+        return result
+
 
     # -----------------调用其他接口动态获得要使用的字段----------------------------------
     def interface_requests_data(self,member_id,urlpart,payload):
