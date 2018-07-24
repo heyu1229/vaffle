9cb22928f5401886d4aa0808d7ea07ae8dbbd900
heyu1229 <1004856404@qq.com>
2018-07-24 10:39:58 +0800

    Merge branch 'master' of https://github.com/heavengifts/test_code
    # Conflicts:
    #	Vaffle_interface/test_date/interface.xls

--- Vaffle_interface/public_1/runtest.py
+++ Vaffle_interface/public_1/runtest.py
@@ -6,8 +6,8 @@ from HTMLTestRunner import HTMLTestRunner
 from email.mime.multipart import MIMEMultipart
 
 my_sender='lisa.he@heavengifts.com' #发件人邮箱账号
-my_user='omv.team@heavengifts.com' #收件人邮箱账号
-# my_user='1004856404@qq.com' #收件人邮箱账号
+#my_user='omv.team@heavengifts.com' #收件人邮箱账号
+my_user='1004856404@qq.com' #收件人邮箱账号
 #发送邮件
 def send_mail(file_new,excel_new):
     f = open(file_new,'rb')
@@ -18,7 +18,7 @@ def send_mail(file_new,excel_new):
         msg = MIMEMultipart('related')
         msg["From"] = my_sender
         msgText = MIMEText(mail_body,'html','utf-8')
-        msg['Subject'] = Header('vaffle 2.4.1 develop接口自动化测试报告','utf-8')
+        msg['Subject'] = Header('vaffle 2.6 develop接口自动化测试报告','utf-8')
         msg.attach(msgText)
 
         # #添加excel附件
