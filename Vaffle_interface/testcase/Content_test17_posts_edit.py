1d1ea254161ba5f3d84c2ae0e14fd0f0693d6603
heyu1229 <1004856404@qq.com>
2018-07-24 10:34:56 +0800

    Merge branch 'master' of https://github.com/heavengifts/test_code
    # Conflicts:
    #	Vaffle_interface/test_date/interface.xls

--- Vaffle_interface/testcase/Content_test17_posts_edit.py
+++ Vaffle_interface/testcase/Content_test17_posts_edit.py
@@ -55,6 +55,29 @@ class PostsDetail(unittest.TestCase):
         self.assertEqual(10003, result["code"])
         print("code返回值：10003")
 
+    #-----------------编辑review----------------------------------
+    def testcase_003(self):
+        sheet_index = 1
+        row = 100
+        print("testcase_001动态编辑：")
+        obj = ({"path":"https://s3-us-west-2.amazonaws.com/images-omv/posts/1532313355871_767_android.jpg","ratio":1.23,"tag":1},)
+        images = json.dumps(obj)
+        # 1.调用发布接口发送一条动态，获取post_id
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        payload1 = {"uid": "744","content":"\u97ed\u83dc\u9e21\u86cb\u5c31<b>\u56de\u5230\u5bb6</b>\u000a\u000a<cite><strong>\u597d\u7684</strong></cite>\u000a\u000a<hr>\u000a\u000a<cite><strong></strong></cite><cite><strong></strong></cite>\u000a\u000a<img src='https://s3-us-west-2.amazonaws.com/images-omv/posts/1532312552416_980_android.jpg' androidid='1532339624238' width='984' height='984'>\u000a\u000a<!--more&lt;cite&gt;&lt;strong&gt;&lt;/strong&gt;&lt;/cite&gt;-->","category":"review","review_title":"接口在"+date+"发布review","review_product":"测评产品","review_type":"测评产品型号","publish_addr":"111"}
+        member_id = "744"
+        urlpart1 = '/posts/publish'
+        result1 = self.r.interface_requests_data(member_id, urlpart1, payload1)
+        global post_id
+        post_id = result1["data"]["post_id"]
+
+        # 2.调用动态编辑接口
+        payload ={'post_id':post_id,'content':"\u97ed\u83dc\u9e21\u86cb\u5c31<b>\u56de\u5230\u5bb6</b>\u000a\u000a<cite><strong>\u597d\u7684</strong></cite>\u000a\u000a<hr>\u000a\u000a<cite><strong></strong></cite><cite><strong></strong></cite>\u000a\u000a<img src='https://s3-us-west-2.amazonaws.com/images-omv/posts/1532312552416_980_android.jpg' androidid='1532339624238' width='984' height='984'>\u000a\u000a<!--more&lt;cite&gt;&lt;strong&gt;&lt;/strong&gt;&lt;/cite&gt;-->","review_title":"接口在"+date+"编辑review","review_product":"测评产品","review_type":"测评产品型号"}
+        result=self.r.interface_requests_payload(member_id, sheet_index, row, payload)
+
+        self.assertEqual(10000, result["code"])
+        print("code返回值：10000")
+
 
 if __name__=="__main__":
     unittest.main()
