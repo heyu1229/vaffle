d9bd56684a80cb736bf3077d1bea4840d89a73da
yuqin <921467314@qq.com>
2018-11-12 17:28:37 +0800

    yuqin

--- Vaffle_interface/testcase/Shop_test4_shop_correction.py
+++ Vaffle_interface/testcase/Shop_test4_shop_correction.py
@@ -17,18 +17,32 @@ from func_requests import FuncRequests
 class Shop(unittest.TestCase):
 
     def setUp(self):
-       self.r=FuncRequests()
+       self.requests = FuncRequests()
+       self.member_id = 'none'
 
     #-----------------店铺纠错信息提交成功----------------------------------
     def testcase_001(self):
         sheet_index = 12
         row = 7
-        self.member_id='746'
+        # 先注册新用户
+        # 先调用注册接口重新注册一个用户
+        nowTime = time.strftime("%Y%m%d_%H_%M_%S")
+        nickname = 'test' + nowTime
+        email = 'test' + nowTime + '@qq.com'
+        print(email)
+        member_id1 = 'none'
+        payload1 = {'email': email, 'password': 'aaa111', 'displayname': 'test', 'nickname': nickname,
+                    'equipment_number': 'PE-TL10', }
+        urlpart1 = '/sign/up'
+        result1 = self.requests.interface_requests_data(member_id1, urlpart1, payload1)
+        member_id = str(result1["data"]["member_id"])
+        print("member_id=", member_id)
+
         print ("testcase_001提交店铺纠错信息:")
         obj = ({"week":[1,2],"start":"01:00","close":"03:00"},{"week":[4,5],"start":"05:00","close":"06:00"})
         business_hours = json.dumps(obj)
         payload = {"shop_id":"1315","tel":"18888066666","address":"address111","shop_name":"shop_name111","business_hours":business_hours,"other":" 111"}
-        result = self.r.interface_requests_payload(self.member_id,sheet_index,row,payload)
+        result = self.requests.interface_requests_payload(member_id,sheet_index,row,payload)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
 
