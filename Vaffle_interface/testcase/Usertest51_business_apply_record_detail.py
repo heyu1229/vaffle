726dd4db005423d49e2b1fea6ce7b5ba325145bf
yuqin <921467314@qq.com>
2019-01-07 15:10:05 +0800

    yuqin

--- Vaffle_interface/testcase/Usertest51_business_apply_record_detail.py
+++ Vaffle_interface/testcase/Usertest51_business_apply_record_detail.py
@@ -20,17 +20,62 @@ class Brands(unittest.TestCase):
        self.r=FuncRequests()
        self.member_id = '746'
 
-    #-----------------查看申请记录的详情信息----------------------------------
+    #-----------------申请店铺管理者的详情信息----------------------------------
     def testcase_001(self):
         sheet_index = 0
         row = 129
 
-        s = "select id from vape_shop_keeper_apply where member_id=746"
+        s = "select a.id from vape_business_log a, vape_shop_keeper_apply b where a.pri_id=b.id and b.member_id=746"
         id = self.r.sql_vaffle_post(s)
+        print('apply_id=', id)
         apply_id = id['id']
-        print('apply_id=', apply_id)
-        print("testcase_001查看申请记录的详情信息：")
+        print("testcase_001 申请店铺管理者的详情信息：")
         payload = {"category":1,"id":apply_id}
+        result = self.r.interface_requests_payload(self.member_id, sheet_index, row,payload)
+        self.assertEqual(10000, result['code'])
+        print("code返回值：10000")
+
+    #-----------------申请品牌管理者的详情信息----------------------------------
+    def testcase_002(self):
+        sheet_index = 0
+        row = 140
+        self.member_id = '745'
+        s = "select a.id from vape_business_log a, vape_brand_keeper_apply b where a.pri_id=b.id and b.member_id=745"
+        id = self.r.sql_vaffle_post(s)
+        print('apply_id=', id)
+        apply_id = id['id']
+        print("testcase_002 申请品牌管理者的详情信息：")
+        payload = {"category":3,"id":apply_id}
+        result = self.r.interface_requests_payload(self.member_id, sheet_index, row,payload)
+        self.assertEqual(10000, result['code'])
+        print("code返回值：10000")
+
+    #-----------------提交修改品牌资料的详情信息----------------------------------
+    def testcase_003(self):
+        sheet_index = 0
+        row = 141
+        self.member_id = '34791'
+        s = "select a.id from vape_business_log a, vape_enterbrand_audit b where a.pri_id=b.id and b.member_id=34791"
+        id = self.r.sql_vaffle_post(s)
+        print('apply_id=', id)
+        apply_id = id['id']
+        print("testcase_003 提交修改品牌资料的详情信息：")
+        payload = {"category":4,"id":apply_id}
+        result = self.r.interface_requests_payload(self.member_id, sheet_index, row,payload)
+        self.assertEqual(10000, result['code'])
+        print("code返回值：10000")
+
+    #-----------------查看申请记录的详情信息----------------------------------
+    def testcase_004(self):
+        sheet_index = 0
+        row = 142
+        self.member_id = '10394'
+        s = "select a.id from vape_business_log a, vape_shops_audit b where a.pri_id=b.id and b.member_id=10394"
+        id = self.r.sql_vaffle_post(s)
+        print('apply_id=', id)
+        apply_id = id['id']
+        print("testcase_004 修改店铺资料的详情信息：")
+        payload = {"category":5,"id":apply_id}
         result = self.r.interface_requests_payload(self.member_id, sheet_index, row,payload)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
