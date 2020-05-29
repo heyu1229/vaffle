3140348f4da21c279ddb9f6f716a5afe00432b32
beth.yu <beth.yu@heavengifts.com>
2020-05-29 11:53:54 +0800

    yuqin05281337

--- Vaffle_ios/testcase/testcase015_grouppost.py
+++ Vaffle_ios/testcase/testcase015_grouppost.py
@@ -11,15 +11,8 @@ import random
 class IOSTest_grouppost(unittest.TestCase):
 
     def setUp(self):
-        # os.system ( 'start startAppiumServer.bat' )
-        time.sleep(10)
-        platformName = 'ios'
-        platformVersion = '11.2.1'
-        deviceName = 'iPhone'
-        udid = '61bb2263cfd0c8847559aa0da3cb6c7e8366f0ce'
-        app = '..//app/Vape.ipa'
         ios = iostest()
-        self.driver=ios.testios(platformName,platformVersion,deviceName,udid,app)
+        self.driver=ios.testios()
 
         self.read = Readdata()
         self.write = Writedata()
@@ -27,7 +20,6 @@ class IOSTest_grouppost(unittest.TestCase):
         self.password = int(self.read.Read_data(0, 2, 1))
         self.password = int(self.read.Read_data(0, 2, 1))
         self.public = Publicway(self.driver)
-        self.driver.implicitly_wait(5)
 
     # ------------------发布群组post-------------------------------------------------------
     def testcase001(self):
@@ -53,7 +45,7 @@ class IOSTest_grouppost(unittest.TestCase):
         self.driver.find_element_by_ios_predicate("name=='back black'").click()
         self.driver.find_element_by_ios_predicate("name == ' - tab - 5 of 5'").click()
         self.driver.find_element_by_accessibility_id("Account").click()
-        time.sleep(2)
+        time.sleep(5)
         self.driver.execute_script('mobile:swipe', {'direction': 'up'})
 
         try:
@@ -64,8 +56,8 @@ class IOSTest_grouppost(unittest.TestCase):
 
         # 断言是否收藏成功
         self.driver.save_screenshot('..//testreport/screenshot/test015_grouppost.jpg')
-        self.assertEqual(1, flag, self.write.Write_data(1, 35, 4, '发布失败'))
-        self.write.Write_data(1, 35, 4, '发布成功')
+        self.assertEqual(1, flag, self.write.Write_data(1, 24, 4, '发布失败'))
+        self.write.Write_data(1, 24, 4, '发布成功')
 
     def tearDown(self):
         self.driver.quit()
