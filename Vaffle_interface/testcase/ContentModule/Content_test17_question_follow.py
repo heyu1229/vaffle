b3d99da93b57d7baaf2b98db6c7311c7d7fdd811
yuqin <921467314@qq.com>
2020-07-24 17:23:54 +0800

    yuqin

--- Vaffle_interface/testcase/ContentModule/Content_test17_question_follow.py
+++ Vaffle_interface/testcase/ContentModule/Content_test17_question_follow.py
@@ -4,17 +4,8 @@ import requests
 import sys
 import json
 import time
+from Vaffle_interface.public_1.func_requests import FuncRequests
 
-import xlrd
-
-import global_list
-sys.path.append(global_list.path+"/public_1")
-from get_url import Url
-from get_version import Version
-from get_token import Token
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from func_requests import FuncRequests
 
 #---------------Q/A的关注和取消关注----------------------
 class CommentsPublish(unittest.TestCase):
@@ -25,15 +16,15 @@ class CommentsPublish(unittest.TestCase):
     #-----------------Q/A的关注----------------------------------
     def testcase_001(self):
         sheet_index = 1
-        row = 69
+        row = 25
         print("testcase_001 Q/A的关注：")
 
         # 调用发布接口发送一条动态，获取post_id
         date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         payload1 = { "content": "接口在" + date + "测试发布Q/A","category":"qa"}
-        member_id1 = "748"
+        member_id = "b9f73f23-7bc6-4de6-9f9b-df2c98076221"
         urlpart = '/posts/publish'
-        result1 = self.r.interface_requests_data(member_id1, urlpart, payload1)
+        result1 = self.r.interface_requests_data(member_id, urlpart, payload1)
         print(result1)
         global question_id
         question_id = result1["data"]["question_id"]
@@ -41,7 +32,6 @@ class CommentsPublish(unittest.TestCase):
 
 
         payload = {"question_id": question_id,"follow_state": 1}
-        member_id="744"
         result=self.r.interface_requests_payload(member_id, sheet_index, row, payload)
 
         self.assertEqual(10000, result["code"])
@@ -50,11 +40,11 @@ class CommentsPublish(unittest.TestCase):
         # -----------------Q/A的取消关注----------------------------------
     def testcase_002(self):
         sheet_index = 1
-        row = 70
+        row = 26
         print("testcase_002 Q/A的取消关注：")
         payload = {"question_id": question_id, "follow_state":0}
         # 获取token值
-        member_id="744"
+        member_id="b9f73f23-7bc6-4de6-9f9b-df2c98076221"
         result=self.r.interface_requests_payload(member_id, sheet_index, row, payload)
 
         self.assertEqual(10000, result["code"])
