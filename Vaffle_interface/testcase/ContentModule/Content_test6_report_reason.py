af0132a488c04c9cb3d1f75236a205ecb530910e
yuqin <921467314@qq.com>
2020-07-24 16:38:04 +0800

    yuqin

--- Vaffle_interface/testcase/ContentModule/Content_test6_report_reason.py
+++ Vaffle_interface/testcase/ContentModule/Content_test6_report_reason.py
@@ -3,15 +3,7 @@ import unittest
 import requests
 import sys,time,gc,xlrd
 import json
-import global_list
-sys.path.append(global_list.path+"/public_1")
-from get_url import Url
-from get_token import Token
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from get_version import Version
-from func_requests import FuncRequests
-
+from Vaffle_interface.public_1.func_requests import FuncRequests
 #---------------举报动态原因----------------------
 class ReportReason(unittest.TestCase):
 
@@ -21,9 +13,9 @@ class ReportReason(unittest.TestCase):
     #-----------------举报动态原因----------------------------------
     def testcase_001(self):
         sheet_index = 1
-        row = 16
+        row = 12
         print("testcase_001举报动态原因：")
-        member_id = "744"
+        member_id = "b9f73f23-7bc6-4de6-9f9b-df2c98076221"
         result=self.r.interface_requests(member_id,sheet_index,row)
 
         self.assertEqual(10000, result["code"])
