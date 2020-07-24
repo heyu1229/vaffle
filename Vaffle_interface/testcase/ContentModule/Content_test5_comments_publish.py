bd68ff734497efa6a5b498f0995323fce3d54625
yuqin <921467314@qq.com>
2020-07-24 15:34:11 +0800

    yuqin

--- Vaffle_interface/testcase/ContentModule/Content_test5_comments_publish.py
+++ Vaffle_interface/testcase/ContentModule/Content_test5_comments_publish.py
@@ -1,90 +1,35 @@
 # -*- coding:UTF-8 -*-
-import unittest
-import requests
-import sys
-import time
-
-import global_list
-from public_1.sql_search import SQL_SEARCH_1
-from public_1.sql_vaffle import SQL_vaffle
-
-sys.path.append(global_list.path+"/public_1")
-from get_url import Url
-from get_version import Version
-from func_requests import FuncRequests
+import unittest,time,json
+from Vaffle_interface.public_1.func_requests import FuncRequests
 
 #---------------发布评论----------------------
-class CommentsPublish(unittest.TestCase):
+class Brands(unittest.TestCase):
 
     def setUp(self):
-        self.r = FuncRequests()
+       self.r=FuncRequests()
 
-    #-----------------批量给post发布评论----------------------------------
+    #-----------------发布评论----------------------------------
     def testcase_001(self):
         sheet_index = 1
-        row = 11
-        i = 38100
-        print("testcase_001发布评论：")
-
-
+        row = 10
         # 调用发布接口发送一条动态，获取post_id
+        member_id = "b9f73f23-7bc6-4de6-9f9b-df2c98076221"
+        obj = ({"path": "posts/1512710644871_767_android.jpg", "ratio": 1.23, "tag": 1},)
+        images = json.dumps(obj)
         date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        self.path = Url().test_path()
-        self.version = Version().test_version()
-        # 路径
-        url = Url().test_url()
-        self.base_url1 =url + '/comments/publish'
-        while i <= 38100:
-            #从数据库取UUID
-            i = i+1
-            sql = SQL_vaffle ().select_uuid (i)
-            uuid = '%s' % SQL_SEARCH_1 ().search ( sql )
+        payload1 = {"content": "接口在" + date + "测试发布post用于发布评论", "images": images, "category": "post"}
+        # 获取发布接口token值
+        urlpart1 = '/posts/publish'
+        result1 = self.r.interface_requests_data(member_id, urlpart1, payload1)
+        post_id = result1["data"]["post_id"]
 
-            payload = {"post_id": "44337","content": "接口在"+date+"1级评论用户ID"+str(i),"is_post":"N"}
-            headers = {"device": "android", "version": "3.8.0", "lang": "en", "timestamp": "1564033489234", "token": "FkUw1pOFkUw1pOBHh7xSI8jWf0X6JuryjWHjhuMapX69FKZSVgBHh7xSI8jWf0X6JuryjWHjhuMapX69FKZSVg",
-                       "uuid": uuid,"serial-number":"48525687125863258471123568955554","company":"HUAWEI","phone-model":"P10","system-version":"8.0.0"}
-            result = requests.post(self.base_url1, params=payload, headers=headers)
-            result = result.json()
-            print(result)
-        #global comment_id
-        #comment_id = result["data"]["comment_id"]
 
-        self.assertEqual(10000, result["code"])
+        print ("testcase_001发布评论:")
+        payload = {'post_id':post_id,"content":"接口测试发布评论内容"+date}
+        result=self.r.interface_requests_payload(member_id, sheet_index, row, payload)
+
+        self.assertEqual(10000, result['code'])
         print("code返回值：10000")
-
-
-    #-----------------批量回复评论----------------------------------
-    def testcase_002(self):
-        sheet_index = 1
-        row = 11
-        i = 38100
-        print("testcase_001发布评论：")
-
-
-        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        self.path = Url().test_path()
-        self.version = Version().test_version()
-        # 路径
-        url = Url().test_url()
-        self.base_url1 =url + '/comments/publish'
-        while i <= 38151:
-            #从数据库取UUID
-            i = i+1
-            sql = SQL_vaffle ().select_uuid (i)
-            uuid = '%s' % SQL_SEARCH_1 ().search ( sql )
-
-            payload = {"post_id": "44337","content": "接口在"+date+"回复评论用户ID"+str(i),"is_post":"N","reply_id":"16965"}
-            headers = {"device": "android ", "version": "3.7.0", "lang": "en", "timestamp": "1564033489234", "token": "FkUw1pOFkUw1pOBHh7xSI8jWf0X6JuryjWHjhuMapX69FKZSVgBHh7xSI8jWf0X6JuryjWHjhuMapX69FKZSVg",
-                       "uuid": uuid,"serial-number":"48525687125863258471123568955554","company":"HUAWEI","phone-model":"P10","system-version":"8.0.0"}
-            result = requests.post(self.base_url1, params=payload, headers=headers)
-            result = result.json()
-            print(result)
-        #global comment_id
-        #comment_id = result["data"]["comment_id"]
-
-        self.assertEqual(10000, result["code"])
-        print("code返回值：10000")
-
 
 if __name__ == "__main__":
     unittest.main()
