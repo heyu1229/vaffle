e24f88d6993cdd7f438ff359c6d5c717843ff691
heyu1229 <1004856404@qq.com>
2020-07-30 13:29:34 +0800

    丽萨

--- Vaffle_interface/testcase/UserModule/Usertest36_business_accountinfo.py
+++ Vaffle_interface/testcase/UserModule/Usertest36_business_accountinfo.py
@@ -4,29 +4,23 @@ import requests
 import sys,time
 import json,xlrd
 # sys.path.append("/usr/lib/python3/heaven_interface_vaffle2.0_auto2/public")
-import global_list
-sys.path.append(global_list.path+"/public_1")
-from get_url import Url
-from get_version import Version
-from get_token import Token
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from func_requests import FuncRequests
-
+from Vaffle_interface.public_1.func_requests import FuncRequests
+from Vaffle_interface.public_1.get_url import Url
 #---------------获取商业级用户的登录信息----------------------
 class Brands(unittest.TestCase):
 
     def setUp(self):
-       self.r=FuncRequests()
-       self.member_id = '745'
+        self.member_uuid = Url ().test_user ()
+        self.requests = FuncRequests ()
 
     #-----------------获取商业级用户的登录信息----------------------------------
     def testcase_001(self):
         sheet_index = 0
-        row = 109
+        row = 36
         print("testcase_001获取商业级用户的登录信息：")
-        payload = {"normal_member_id":745,"target_role":10394,"fcm_token":"fcm_token"}
-        result = self.r.interface_requests_payload(self.member_id, sheet_index, row,payload)
+        payload = {"normal_member_uuid":"a5f10151-5685-4432-8c35-7198bc6511c9",
+                   "target_role_uuid":"cafe166a-8842-457e-8467-82f18d00275d","fcm_token":"fcm_token"}
+        result = self.requests.interface_requests_payload(self.member_uuid, sheet_index, row,payload)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
 
