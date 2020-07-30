19693648d377b5c6a45093fa4faf567bbf6202c9
heyu1229 <1004856404@qq.com>
2020-07-30 18:01:29 +0800

    丽萨

---
+++ Vaffle_interface/testcase/UserModule/Usertest53_passport_retrievepasswordways.py
@@ -0,0 +1,27 @@
+# -*- coding:UTF-8 -*-
+import unittest
+import requests
+import sys,time
+import json,xlrd
+
+from Vaffle_interface.public_1.func_requests import FuncRequests
+from Vaffle_interface.public_1.get_url import Url
+
+#---------------找回密码方式【POST】---------------------
+class Passport_retrievepasswordways(unittest.TestCase):
+
+    def setUp(self):
+        self.member_uuid = Url ().test_user ()
+        self.requests = FuncRequests ()
+
+
+    def testcase_001(self):
+        sheet_index = 0
+        row = 53
+        print("testcase_001 找回密码方式【POST】：")
+        result = self.requests.interface_requests(self.member_uuid,sheet_index,row)
+        self.assertEqual(10000, result['code'])
+        print("code返回值：10000")
+
+if __name__ == "__main__":
+    unittest.main()
