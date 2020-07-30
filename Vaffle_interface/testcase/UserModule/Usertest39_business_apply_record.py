1159a3b2208fbf9bfce1a4d7d37ba0f9227b92dd
heyu1229 <1004856404@qq.com>
2020-07-30 14:14:43 +0800

    丽萨

--- Vaffle_interface/testcase/UserModule/Usertest39_business_apply_record.py
+++ Vaffle_interface/testcase/UserModule/Usertest39_business_apply_record.py
@@ -5,23 +5,25 @@ import sys,time
 import json,xlrd
 from Vaffle_interface.public_1.func_requests import FuncRequests
 from Vaffle_interface.public_1.get_url import Url
-#---------------获取商业级用户的登录信息----------------------
+
+#---------------申请店长、品牌管理记录----------------------
 class Brands(unittest.TestCase):
 
     def setUp(self):
         self.member_uuid = Url ().test_user ()
         self.requests = FuncRequests ()
 
-    #-----------------获取商业级用户的登录信息----------------------------------
+
+    #-----------------店铺管理员查看记录----------------------------------
     def testcase_001(self):
         sheet_index = 0
-        row = 36
-        print("testcase_001获取商业级用户的登录信息：")
-        payload = {"normal_member_uuid":"a5f10151-5685-4432-8c35-7198bc6511c9",
-                   "target_role_uuid":"cafe166a-8842-457e-8467-82f18d00275d","fcm_token":"fcm_token"}
+        row = 39
+        print("testcase_001店铺管理员查看记录：")
+        payload = {"normal_member_uuid":self.member_uuid,"page":1}
         result = self.requests.interface_requests_payload(self.member_uuid, sheet_index, row,payload)
         self.assertEqual(10000, result['code'])
         print("code返回值：10000")
+
 
 if __name__ == "__main__":
     unittest.main()
