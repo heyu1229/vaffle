52f6bb002e7216cba5faa1a7d56775b13e30eb4b
heyu1229 <1004856404@qq.com>
2020-08-14 14:56:35 +0800

    丽萨

--- Vaffle_interface/testcase/WalletModule/Wallet6_wallet_account_lists.py
+++ Vaffle_interface/testcase/WalletModule/Wallet6_wallet_account_lists.py
@@ -1,34 +1,46 @@
 # -*- coding:UTF-8 -*-
 import unittest
 import requests
-import sys,time
-import json,xlrd
-# sys.path.append("/usr/lib/python3/heaven_interface_vaffle2.0_auto2/public")
-import global_list
-sys.path.append(global_list.path+"/public_1")
-from get_url import Url
-from get_version import Version
-from get_token import Token
-from read_data import Read_ExcelData
-from write_data import Write_ExcelData
-from func_requests import FuncRequests
+import time,gc,sys
 
-#---------------钱包 - 可提现的账户列表----------------------
-class Brands(unittest.TestCase):
+#------------------------钱包 - 提现记录---------------------------
+from Vaffle_interface.public_1.func_requests import FuncRequests
+from Vaffle_interface.public_1.get_url import Url
+
+
+class wallet_income(unittest.TestCase):
 
     def setUp(self):
-       self.r=FuncRequests()
-       self.member_id = '10394'
+        self.member_uuid = Url().test_user()
+        self.requests = FuncRequests()
 
-    #-----------------钱包 - 可提现的账户列表----------------------------------
     def testcase_001(self):
-        sheet_index = 13
-        row = 8
-        print("testcase_001钱包 - 可提现的账户列表：")
-
-        result = self.r.interface_requests(self.member_id, sheet_index, row)
-        self.assertEqual(10000, result['code'])
+        sheet_index =1
+        row = 6
+        result = self.requests.interface_requests(self.member_uuid,sheet_index,row)
+        self.assertEqual(10000, result["code"])
         print("code返回值：10000")
+
+
+
+    #-----------------钱包 - 提现账户申请----------------------------------
+    # def testcase_001(self):
+    #
+    #     s = "delete from vape_reflect_record where member_id='70493'"
+    #     execute_sql = self.requests.sql_vaffle(s)
+    #     s1="update vape_wallet set balance=100,frozen_money=0 where member_id='70493'"
+    #     execute_sql1 = self.requests.sql_vaffle(s1)
+    #     #1.因为提现最小金额是50，所以要先去数据库将balane改成比frozen_money大50才能执行该用例
+    #     #2.因为一个月只能提现一次，所以要先删除数据库中表vape_reflect_record中该用户的当月提现记录
+    #     sheet_index = 1
+    #     row = 6
+    #     print("testcase_001钱包 - 发起提现申请：")
+    #
+    #     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    #     payload = {"account":date,"money":"50"}
+    #     result = self.requests.interface_requests_payload(self.member_uuid, sheet_index, row, payload)
+    #     self.assertEqual(10000, result['code'])
+    #     print("code返回值：10000")
 
 if __name__ == "__main__":
     unittest.main()
