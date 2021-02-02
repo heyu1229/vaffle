170a7ddbc09f6e0567b6f697e2a39f191675185b
yuqin <921467314@qq.com>
2021-02-02 16:11:04 +0800

    yuqin

---
+++ Vaffle_interface/testcase/ContentModule/Content_test81_goods_comments_lists.py
@@ -0,0 +1,27 @@
+# -*- coding:UTF-8 -*-
+import unittest,time
+from Vaffle_interface.public_1.func_requests import FuncRequests
+
+#---------------问题删除功能---------------------
+from Vaffle_interface.public_1.func_requests import FuncRequests
+from Vaffle_interface.public_1.get_url import Url
+
+#-----------------标签详情页-----------
+class Question_edit(unittest.TestCase):
+
+    def setUp(self):
+        self.r = FuncRequests()
+        self.member_uuid = Url().test_user()
+
+    def testcase_001(self):
+        sheet_index = 1
+        row = 101
+
+        payload = {"page": 1,"qaLabelTitle":"Mod"}
+        result = self.r.interface_requests_payload(self.member_uuid, sheet_index, row, payload)
+        self.assertEqual(10000, result["code"])
+        print("code返回值：10000")
+
+
+if __name__ == "__main__":
+    unittest.main()
