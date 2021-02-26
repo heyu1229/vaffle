57cff8dfa4b1cda3ba2488398b114fe0c4881cd1
heyu1229 <1004856404@qq.com>
2021-02-26 13:48:55 +0800

    丽萨

---
+++ Vaffle_interface/testcase/ActivityModule/web11_elfbar_get_daily_gifts.py
@@ -0,0 +1,32 @@
+'''
+@create : lisa
+@file :web5_elfbar_reserve.py
+@Date :2021/2/26
+@desc :
+
+'''
+# -*- coding:UTF-8 -*-
+import unittest
+import requests
+import sys,time
+import json,xlrd
+
+from Vaffle_interface.public_1.func_requests import FuncRequests
+from Vaffle_interface.public_1.get_url import Url
+#---------------【H5】elfbar活动-每日奖励【POST】--------------------
+class web_elfbar_get_daily_gifts(unittest.TestCase):
+
+    def setUp(self):
+        self.member_uuid = Url ().test_user ()
+        self.requests = FuncRequests ()
+
+
+    def testcase_001(self):
+        sheet_index = 16
+        row = 11
+        result = self.requests.interface_requests(self.member_uuid,sheet_index,row)
+        self.assertEqual(10000, result['code'])
+        print("code返回值：10000")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Vaffle_interface/testcase/ActivityModule/web1_elfbar_home.py
@@ -0,0 +1,33 @@
+'''
+@create : lisa
+@file :web1_elfbar_home.py
+@Date :2021/2/26
+@desc :
+
+'''
+# -*- coding:UTF-8 -*-
+import unittest
+import requests
+import sys,time
+import json,xlrd
+
+from Vaffle_interface.public_1.func_requests import FuncRequests
+from Vaffle_interface.public_1.get_url import Url
+
+#---------------【H5】elfbar活动-反馈【POST】--------------------
+class web_elfbar_home(unittest.TestCase):
+
+    def setUp(self):
+        self.member_uuid = Url ().test_user ()
+        self.requests = FuncRequests ()
+
+
+    def testcase_001(self):
+        sheet_index = 16
+        row = 1
+        result = self.requests.interface_requests(self.member_uuid,sheet_index,row)
+        self.assertEqual(10000, result['code'])
+        print("code返回值：10000")
+
+if __name__ == "__main__":
+    unittest.main()
